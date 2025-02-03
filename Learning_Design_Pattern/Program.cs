3da4cf11acd4ba4832426e0a1401dc81b40d3edc
Navneet Singh <navneet.singh@epicor.com>
2025-02-03 13:35:11 +0530

    Initial commit

---
+++ Learning_Design_Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Subscribe
+{
+    public string? _name { get; set; }
+    public string? _email { get; set; }
+
+    public Subscribe(string name , string email)
+    {
+        _name = name;
+        _email = email;
+    }
+
+
+}
+
+public class EmailNotification
+{
+    Subscribe? _subscribe;
+    public string? subscribed { get; set; }
+}
+
+public class SMSNotification
+{
+    Subscribe? _subscribe;
+    public string? messagedelived { get; set; }
+}
+
+
---
+++ Learning_Design_Pattern/Singleton_pattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Design_Pattern
+{
+    //Singleton class should be sealed. Not static.
+    //A static class cannot implement an interface, while a Singleton class can.
+    //This is important for dependency injection and unit testing.
+    //Singleton supports interfaces, allowing flexibility in design.Static classes do not allow this.
+    //In ASP.NET Core, Singleton objects can be registered and injected using DI containers,
+    //but static classes cannot.
+    //Static classes cannot be inherited, whereas Singleton classes can be extended or mocked for unit testing.
+    //Singletons allow inheritance and can be mocked for testing.
+    //A static class lives as long as the application and is initialized once at startup.
+    //A Singleton can be disposed and recreated when necessary
+    //Static classes cannot be reset or reinitialized.
+
+    /// <summary>
+    /// Singleton Class used to prevent external creation of object.
+    /// </summary>
+    public sealed class Singleton_pattern
+    {
+        public static  Singleton_pattern singleton_Pattern =null;
+        static Singleton_pattern()
+        {
+            Console.WriteLine($"Single constructor invoked");
+        }
+
+        public static Singleton_pattern GetInstance()
+        {
+            if (singleton_Pattern == null)
+                return new Singleton_pattern();
+            else
+              return   singleton_Pattern;
+              
+        }
+    }
+}
